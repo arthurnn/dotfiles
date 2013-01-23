78fb792eaaa7843aec233510c8f6827eb28bdac4
Arthur Neves <arthurnn@gmail.com>
2013-01-23 14:08:57 -0500

    backup files on emacs config

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -24,3 +24,12 @@
 (defun my-html-mode-hook ()
   (auto-fill-mode -1))
 (add-hook 'html-mode-hook 'my-html-mode-hook)
+
+;; send backup files to its own dir
+(setq backup-directory-alist '(("." . "~/.emacs.d/backup"))
+  backup-by-copying t    ; Don't delink hardlinks
+  version-control t      ; Use version numbers on backups
+  delete-old-versions t  ; Automatically delete excess backups
+  kept-new-versions 20   ; how many of the newest versions to keep
+  kept-old-versions 5    ; and how many of the old
+)
