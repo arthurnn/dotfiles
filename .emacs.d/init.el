36fb73fff3724ae838902922d138622f7ede9a58
Arthur Neves <arthurnn@gmail.com>
2013-09-17 14:18:13 -0400

    use theme instead of colors

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -2,8 +2,7 @@
 (add-to-list 'load-path "~/.emacs.d/init.d/")
 (load "01_packages")
 
-(load-theme 'solarized-dark t)
-
+(load "setup-colors")
 (load "setup-tmux")
 (load "setup-ruby")
 (load "setup-etc")
