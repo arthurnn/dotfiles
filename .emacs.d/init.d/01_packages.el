2ffd8ef4efc91d72edb4c885db1043ebd7a94ffb
Arthur Neves <arthurnn@gmail.com>
2013-11-16 16:55:03 -0500

    Comment ou marmalade package

--- .emacs.d/init.d/01_packages.el
+++ .emacs.d/init.d/01_packages.el
@@ -1,7 +1,7 @@
 (require 'cl)
 (require 'package)
 (add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/"))
-(add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/"))
+;;(add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/"))
 
 (package-initialize)
 
