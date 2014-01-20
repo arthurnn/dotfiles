bc6749486432cd67cf6a612683f4430b2de0b375
Arthur Neves <arthurnn@gmail.com>
2014-01-20 12:30:40 -0500

    add track mode

--- .emacs.d/init.d/setup-erc.el
+++ .emacs.d/init.d/setup-erc.el
@@ -3,6 +3,8 @@
 ;; Freenode.
 (setq erc-keywords '("@arthurnn" "@all" "@everyone"))
 (setq erc-autojoin-channels-alist '(("freenode.net" "#mongoid" "#emacs" "#mongodb" "#24pullrequests")))
+
+(erc-track-mode t)
 (setq erc-track-exclude-types '("JOIN" "NICK" "PART" "QUIT" "MODE"
                                 "324" "329" "332" "333" "353" "477"))
 
