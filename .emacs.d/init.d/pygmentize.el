82380b6af6f954b577201869b03c6f96558b0dc7
Arthur Neves <arthurnn@gmail.com>
2014-05-04 12:26:13 -0400

    Guess the lexer on pygmentize

--- .emacs.d/init.d/pygmentize.el
+++ .emacs.d/init.d/pygmentize.el
@@ -3,6 +3,6 @@
   (interactive)
   (let ((code (buffer-substring (region-beginning) (region-end))))
     (kill-new
-	   (shell-command-to-string (format "echo \"%s\" | /usr/local/bin/pygmentize -f rtf -O \"style=emacs,fontface=Menlo\" -l html" code)))
+	   (shell-command-to-string (format "echo \"%s\" | pygmentize -f rtf -O \"style=emacs,fontface=Menlo\" -l $(pygmentize -N %s)" code buffer-file-name)))
 ))
 (provide 'pygmentize)
