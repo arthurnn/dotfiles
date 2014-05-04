103d8a08102f8ef4e9448ac511e7618c5666d961
Arthur Neves <arthurnn@gmail.com>
2014-05-04 08:46:05 -0300

    Fix indentation

--- .emacs.d/darwin.el
+++ .emacs.d/darwin.el
@@ -4,13 +4,13 @@
 
 ;; SHARE CLIPBOARD WITH EMACS
 (defun copy-from-osx ()
-    (shell-command-to-string "pbpaste"))
+  (shell-command-to-string "pbpaste"))
 
 (defun paste-to-osx (text &optional push)
-    (let ((process-connection-type nil))
-            (let ((proc (start-process "pbcopy" "*Messages*" "pbcopy")))
-                      (process-send-string proc text)
-                              (process-send-eof proc))))
+  (let ((process-connection-type nil))
+    (let ((proc (start-process "pbcopy" "*Messages*" "pbcopy")))
+      (process-send-string proc text)
+      (process-send-eof proc))))
 
 (unless (getenv "TMUX")
   (progn
@@ -18,8 +18,8 @@
     (setq interprogram-paste-function 'copy-from-osx)))
 
 (setq browse-url-browser-function
-        'browse-url-generic browse-url-generic-program
-        "open")
+      'browse-url-generic browse-url-generic-program
+      "open")
 
 (provide 'darwin)
 ;;; darwin.el ends here
