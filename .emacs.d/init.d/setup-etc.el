de2b533b8c91781ea90f321e941ba7b3355e341d
Arthur Neves <arthurnn@gmail.com>
2014-05-06 14:07:22 -0400

    Add color to css
    tip from http://ergoemacs.org/emacs/emacs_CSS_colors.html

--- .emacs.d/init.d/setup-etc.el
+++ .emacs.d/init.d/setup-etc.el
@@ -110,4 +110,19 @@ If the current buffer is not associated with a file, do nothing."
  '(change-log-default-name "CHANGELOG.md")
 )
 
+(defun xah-syntax-color-hex ()
+"Syntax color hex color spec such as 「#ff1100」 in current buffer."
+  (interactive)
+  (font-lock-add-keywords
+   nil
+   '(("#[abcdef[:digit:]]\\{6\\}"
+      (0 (put-text-property
+          (match-beginning 0)
+          (match-end 0)
+          'face (list :background (match-string-no-properties 0)))))))
+  (font-lock-fontify-buffer))
+(add-hook 'css-mode-hook 'xah-syntax-color-hex)
+(add-hook 'php-mode-hook 'xah-syntax-color-hex)
+(add-hook 'html-mode-hook 'xah-syntax-color-hex)
+
 (provide 'setup-etc)
