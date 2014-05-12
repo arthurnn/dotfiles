18dd4b342aab21c0e583bca575e9f4c5878f4d6a
Arthur Neves <arthurnn@gmail.com>
2014-05-12 18:00:28 -0400

    Remove marmalade for now

--- .emacs.d/init.d/01_packages.el
+++ .emacs.d/init.d/01_packages.el
@@ -19,7 +19,6 @@
 )  "A list of packages to ensure are installed at launch.")
 
 (setq package-archives '(("gnu" . "http://elpa.gnu.org/packages/")
-                         ("marmalade" . "http://marmalade-repo.org/packages/")
                          ("melpa" . "http://melpa.milkbox.net/packages/")))
 
 (package-initialize)
