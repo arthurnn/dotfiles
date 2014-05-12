0895a2a4b9db4d34d1a46fec881a426bd9024713
Arthur Neves <arthurnn@gmail.com>
2014-05-12 15:57:36 -0400

    Remove emacs WARN

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -10,7 +10,6 @@
 ;; Load path etc.
 (setq dotfiles-dir (file-name-directory
                     (or (buffer-file-name) load-file-name)))
-(add-to-list 'load-path dotfiles-dir)
 (add-to-list 'load-path (concat dotfiles-dir "/init.d"))
 
 ;; Custom libs
