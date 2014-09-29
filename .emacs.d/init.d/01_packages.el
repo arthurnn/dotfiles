4c487d57aae42aa429a41e1d60300e736c011966
Arthur Neves <arthurnn@gmail.com>
2014-09-28 21:36:00 -0400

    Add new emacs packages

--- .emacs.d/init.d/01_packages.el
+++ .emacs.d/init.d/01_packages.el
@@ -8,7 +8,7 @@
 		      go-mode markdown-mode znc
 		      flx-ido grizzl
 		      projectile projectile-rails
-		      pushbullet
+		      pushbullet ag web-beautify
                       ;Themes
                       color-theme color-theme-solarized zenburn-theme
                       ;Ruby
