bd9c97e36550c69bb0a8e0aa6eecac21ec470ef8
Arthur Neves <arthurnn@gmail.com>
2015-05-15 15:12:54 +0000

    Enable projectile cache again. Seems like it works now

--- .emacs.d/init.d/setup-projectile.el
+++ .emacs.d/init.d/setup-projectile.el
@@ -12,7 +12,7 @@
 (add-to-list 'projectile-globally-ignored-directories "node_modules")
 
 (setq projectile-use-git-grep t)
-;;(setq projectile-enable-caching t) ;; THIS DOESNT WORK !
+(setq projectile-enable-caching t)
 (setq projectile-completion-system 'grizzl)
 
 (provide 'setup-projectile)
