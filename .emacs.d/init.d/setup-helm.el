64716eacc34df83038870010cde18738be982655
Arthur Neves <arthurnn@gmail.com>
2015-10-26 11:48:52 -0400

    more bindings for helm mode

--- .emacs.d/init.d/setup-helm.el
+++ .emacs.d/init.d/setup-helm.el
@@ -6,6 +6,9 @@
 (require 'helm-config)
 (helm-mode 1)
 (global-set-key (kbd "C-x C-f") 'helm-find-files)
+(define-key helm-map (kbd "<tab>") 'helm-execute-persistent-action) ; rebind tab to run persistent action
+(define-key helm-map (kbd "C-i") 'helm-execute-persistent-action) ; make TAB works in terminal
+(define-key helm-map (kbd "C-z")  'helm-select-action) ; list actions using C-z
 
 (unless (boundp 'completion-in-region-function)
   (define-key lisp-interaction-mode-map [remap completion-at-point] 'helm-lisp-completion-at-point)
