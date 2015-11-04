9053aeeba744dce732a0f39cf410a936d57bc0e9
Arthur Neves <arthurnn@gmail.com>
2015-11-04 16:33:44 -0500

    replace c-x b with helm

--- .emacs.d/init.d/setup-helm.el
+++ .emacs.d/init.d/setup-helm.el
@@ -6,6 +6,7 @@
 (require 'helm-config)
 (helm-mode 1)
 (global-set-key (kbd "C-x C-f") 'helm-find-files)
+(global-set-key (kbd "C-x b") 'helm-buffers-list)
 (define-key helm-map (kbd "<tab>") 'helm-execute-persistent-action) ; rebind tab to run persistent action
 (define-key helm-map (kbd "C-i") 'helm-execute-persistent-action) ; make TAB works in terminal
 (define-key helm-map (kbd "C-z")  'helm-select-action) ; list actions using C-z
