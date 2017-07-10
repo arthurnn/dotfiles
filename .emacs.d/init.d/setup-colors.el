3733ceba7e247c8744767bba0b9f2bf511d6f79b
Arthur Neves <arthurnn@gmail.com>
2017-07-10 16:41:21 -0400

    whatever

--- .emacs.d/init.d/setup-colors.el
+++ .emacs.d/init.d/setup-colors.el
@@ -3,18 +3,4 @@
 
 (load-theme 'atom-one-dark t)
 
-(let ((dark (eq 'dark (frame-parameter nil 'background-mode))))
-    (set-face-attribute 'whitespace-space nil
-                        :foreground (if dark "pink4" "azure3")
-                        :background 'unspecified)
-    (set-face-attribute 'whitespace-tab nil
-                        :foreground "red"
-                        :background 'unspecified)
-    (set-face-attribute 'whitespace-newline nil
-                        :foreground "gray15")
-    (set-face-attribute 'whitespace-trailing nil
-                        :foreground "red"
-                        :background 'unspecified
-                        :underline t))
-
 (provide 'setup-colors)
