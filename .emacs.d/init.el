dab973a9a344942a08268b6a1a597183a3b51284
Arthur Neves <arthurnn@gmail.com>
2018-02-12 16:38:56 +0100

    Add go path to shell

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -14,6 +14,7 @@
 (require 'exec-path-from-shell)
 (when (memq window-system '(mac ns))
   (exec-path-from-shell-initialize))
+(exec-path-from-shell-copy-env "GOPATH")
 
 ;; Custom libs
 (autoload 'growl "growl" t)
