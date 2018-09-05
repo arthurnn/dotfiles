862bf13c383880b6955f0eec74da09a84c513e3e
Arthur Neves <arthurnn@gmail.com>
2018-09-05 14:36:49 -0400

    Add hideshow for ruby

--- .emacs.d/init.d/setup-ruby.el
+++ .emacs.d/init.d/setup-ruby.el
@@ -44,6 +44,21 @@
 )
 (global-set-key (kbd "C-c r t") 'ruby-erb-t)
 
+;; hide and show ruby blocks
+(add-hook 'ruby-mode-hook
+  (lambda () (hs-minor-mode)))
+
+(eval-after-load "hideshow"
+  '(add-to-list 'hs-special-modes-alist
+    `(ruby-mode
+      ,(rx (or "def" "class" "module" "do" "{" "[")) ; Block start
+      ,(rx (or "}" "]" "end"))                       ; Block end
+      ,(rx (or "#" "=begin"))                        ; Comment start
+      ruby-forward-sexp nil)))
+
+(global-set-key (kbd "C-c h") 'hs-hide-block)
+(global-set-key (kbd "C-c s") 'hs-show-block)
+
 (defun ruby-interpolation-insert ()
   "Called when interpolation key is pressed"
   (interactive)
