cb5a273b10e89d45fcb3ecaf9b8c171bcbb98eb4
Arthur Neves <arthurnn@gmail.com>
2019-12-16 16:06:46 -0500

    use lsp for jump

--- .emacs.d/init.d/setup-go.el
+++ .emacs.d/init.d/setup-go.el
@@ -1,14 +1,12 @@
 ;; Install the following packges:
 ;;
 ;; go get -u golang.org/x/tools/cmd/goimports
-;; go get -u golang.org/x/tools/cmd/guru
-;; go get -u github.com/nsf/gocode
-;; go get -u github.com/josharian/impl
+;; go get golang.org/x/tools/gopls@latest
 
 (require 'company-lsp)
 (push 'company-lsp company-backends)
 
-(add-hook 'go-mode-hook 'go-eldoc-setup)
+;;(add-hook 'go-mode-hook 'go-eldoc-setup)
 
 (defun my-go-mode-hook ()
   ; Use goimports instead of go-fmt
@@ -23,21 +21,12 @@
            "go build -v && go test -v && go vet"))
 
   ; Godef jump key binding
-  (local-set-key (kbd "C-c C-j") 'go-guru-definition)
+  (local-set-key (kbd "C-c C-j") 'lsp-find-definition)
   (local-set-key (kbd "C-c C-b") 'pop-tag-mark)
 )
 (add-hook 'go-mode-hook 'my-go-mode-hook)
 
-(add-hook 'go-mode-hook #'go-guru-hl-identifier-mode)
+(add-hook 'go-mode-hook 'lsp-deferred)
 
-(defun coder/go-guru-set-current-package-as-main ()
-  "GoGuru requires the scope to be set to a go package which
-   contains a main, this function will make the current package the
-   active go guru scope, assuming it contains a main"
-  (interactive)
-  (let* ((filename (buffer-file-name))
-         (gopath-src-path (concat (file-name-as-directory (go-guess-gopath)) "src"))
-         (relative-package-path (directory-file-name (file-name-directory (file-relative-name filename gopath-src-path)))))
-    (setq go-guru-scope relative-package-path)))
 
 (provide 'setup-go)
